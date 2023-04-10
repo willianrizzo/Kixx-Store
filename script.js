b9a31ae1a609d3b29bc0bcb442d27b78cc11bb8a
willianrizzo <willianrizzoratao10@gmail.com>
2023-04-09 23:01:03 -0300

    modais completos

--- script.js
+++ script.js
@@ -17,10 +17,10 @@ const toggleModal = () => {
 const openModalButton2 = document.querySelector("#open-modal2");
 const closeModalButton2 = document.querySelector("#close-modal2");
 const modal2 = document.querySelector("#modal2");
-const fade2 = document.querySelector("#fade");
+const fade2 = document.querySelector("#fade2");
 
 const toggleModal2 = () => {
-  modal.classList.toggle("hide");
+  modal2.classList.toggle("hide");
   fade.classList.toggle("hide");
 };
 
@@ -33,13 +33,61 @@ const toggleModal2 = () => {
 const openModalButton3 = document.querySelector("#open-modal3");
 const closeModalButton3 = document.querySelector("#close-modal3");
 const modal3 = document.querySelector("#modal3");
-const fade3 = document.querySelector("#fade");
+const fade3 = document.querySelector("#fade3");
 
 const toggleModal3 = () => {
-  modal.classList.toggle("hide");
+  modal3.classList.toggle("hide");
   fade.classList.toggle("hide");
 };
 
-[openModalButton2, closeModalButton2, fade2].forEach((el) => {
+[openModalButton3, closeModalButton3, fade3].forEach((el) => {
   el.addEventListener("click", () => toggleModal3());
+});
+
+/////////////
+
+const openModalButton4 = document.querySelector("#open-modal4");
+const closeModalButton4 = document.querySelector("#close-modal4");
+const modal4 = document.querySelector("#modal4");
+const fade4 = document.querySelector("#fade4");
+
+const toggleModal4 = () => {
+  modal4.classList.toggle("hide");
+  fade.classList.toggle("hide");
+};
+
+[openModalButton4, closeModalButton4, fade4].forEach((el) => {
+  el.addEventListener("click", () => toggleModal4());
+});
+
+/////////////
+
+const openModalButton5 = document.querySelector("#open-modal5");
+const closeModalButton5 = document.querySelector("#close-modal5");
+const modal5 = document.querySelector("#modal5");
+const fade5 = document.querySelector("#fade5");
+
+const toggleModal5 = () => {
+  modal5.classList.toggle("hide");
+  fade.classList.toggle("hide");
+};
+
+[openModalButton5, closeModalButton5, fade5].forEach((el) => {
+  el.addEventListener("click", () => toggleModal5());
+});
+
+/////////////
+
+const openModalButton6 = document.querySelector("#open-modal6");
+const closeModalButton6 = document.querySelector("#close-modal6");
+const modal6 = document.querySelector("#modal6");
+const fade6 = document.querySelector("#fade6");
+
+const toggleModal6 = () => {
+  modal6.classList.toggle("hide");
+  fade.classList.toggle("hide");
+};
+
+[openModalButton6, closeModalButton6, fade6].forEach((el) => {
+  el.addEventListener("click", () => toggleModal6());
 });
